81e6858ba0cacc52aafc6a92b5b4e599dcf96595
leehokyun <urea42@naver.com>
2024-11-12 12:37:18 +0900

    [add] tree 위치 중복되지 않는 코드 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -34,61 +34,48 @@ public class GameManager : MonoBehaviour
         MakeTree();
     }
 
-    //void MakeRoad()
-    //{
-    //    int spawnAmount = Random.Range(3, 6); //3이상 6미만 횟수만큼 생성이 반복된다.
-    //    for (int i = 0; i < spawnAmount; i++)
-    //    {
-    //        int[] spawnYPosArr = new int[] { -1, 0, 1, 2, 3, 4 };
-    //        Vector3[] spawnPosArr = new Vector3[spawnAmount];
-
-    //        for (int j = 0; j < spawnYPosArr.Length; j++)
-    //        {
-    //            int spawnYPos = spawnYPosArr[Random.Range(0, spawnYPosArr.Length)]; //randomYPos변수에 spawnYPosArr중 한 값이 랜덤으로 저장됨
-    //            Vector3 spawnPos = new Vector3(0, spawnYPos, 0);
-    //            spawnPosArr[j] = spawnPos;
-    //            Instantiate(road, spawnPosArr[j], Quaternion.identity);
-    //        }
-    //    }
-    //}
     void MakeRoad()
     {
         int spawnAmount = Random.Range(3, 6); //스폰할 양(랜덤)
         List<int> spawnYPosList = new List<int> { -1, 0, 1, 2, 3, 4 }; //스폰될 Y위치값 int 배열
 
-        for (int j = 0; j < spawnAmount; j++) //스폰될 양만큼 반복하여 생성
+        for (int i = 0; i < spawnAmount; i++) //스폰될 양만큼 반복하여 생성
         {
             int spawnYPos = spawnYPosList[Random.Range(0, spawnYPosList.Count)]; //randomYPos에 랜덤하게 특정 스폰Y값이 배정됨
             Vector3 spawnPos = new Vector3(0, spawnYPos, 0); //스폰 위치 세팅 
-            spawnYPosList.Remove(spawnYPos); //위치값이 겹치지 않도록 배열에서 값을 삭제.
+            spawnYPosList.Remove(spawnYPos); //위치값이 겹치지 않도록 배열에서 방금 쓴 값을 삭제.
             Instantiate(road, spawnPos, Quaternion.identity); //생성
         }
     }
 
-    //void MakeMonster()
-    //{
-    //    SetSpawnPos();
-    //    Instantiate(monster, spawnPos1, Quaternion.identity);
-    //}
+    void MakeMonster()
+    {
+        Debug.Log("몬스터생성");
+    }
 
     void MakeTree()
     {
-        int spawnAmount = Random.Range(3, 6); //3이상 6미만 횟수만큼 생성이 반복된다.
+        int spawnAmount = Random.Range(8, 15); //스폰할 양(랜덤)
+        List<int> spawnXPosList = new List<int> { -2, -1, 0, 1, 2 }; //스폰될 X위치값 int 배열
+        List<int> spawnYPosList = new List<int> { -1, 0, 1, 2, 3, 4 }; //스폰될 Y위치값 int 배열
+        List<Vector3> spawnPosList = new List<Vector3>();
+
+        //방법 고안: Dictionary 전체 범위를 세팅하는 for문을 만들고 -> 그 다음 거기서 겹치는 걸 뺀다.
+
         for (int i = 0; i < spawnAmount; i++)
         {
-            int spawnYPos = Random.Range(-1, 5);
-            int spawnXPos = Random.Range(-2, 3);
-            Vector3 spawnPos = new Vector3(spawnXPos, spawnYPos, 0);
-            Instantiate(tree, spawnPos, Quaternion.identity);
+            int spawnXPos = spawnXPosList[Random.Range(0, spawnXPosList.Count)];//randomXPos에 랜덤하게 특정 스폰X값이 배정됨
+            int spawnYPos = spawnYPosList[Random.Range(0, spawnYPosList.Count)]; //randomYPos에 랜덤하게 특정 스폰Y값이 배정됨
+            Vector3 spawnPos = new Vector3(spawnXPos, spawnYPos, 0); //스폰 위치 세팅 
+
+            if (spawnPosList.Contains(spawnPos))
+            {
+                continue;
+            }
+            spawnPosList.Add(spawnPos);
+            Instantiate(tree, spawnPos, Quaternion.identity); //생성
         }
     }
 
-    //void SetSpawnPos()
-    //{
-    //    int[] spawnYPosArr = new int[] { -1, 0, 1, 2, 3, 4 };
-    //    int spawnYPos1 = spawnYPosArr[Random.Range(0, spawnYPosArr.Length)]; //randomYPos변수에 spawnYPosArr중 한 값이 랜덤으로 저장됨
-
-    //    spawnPos1 = new Vector3(0, spawnYPos1, 0);
-    //}
 
 }
