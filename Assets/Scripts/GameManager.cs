1dc1ab49aabb0f7478544ed1ac146b82f58ec4aa
leehokyun <urea42@naver.com>
2024-11-12 12:05:33 +0900

    [add] MakeRoad메서드 중복 생성 방지 코드

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,6 +18,8 @@ public class GameManager : MonoBehaviour
     // Road가 3~5개 생성되어야한다.(완료)
         //Random range 3~5사이의 변수만큼 for문이 돈다. (완료)
         //그 횟수만큼 Road가 Instantiate된다. (완료)
+        //spawnYPos가 겹치지 않아야한다. 그러기위해서는 배열값들이 한번 쓸때마다 제거되어야한다.
+
 
     // Road마다의 위치 값을 받아와야 한다.(전역변수)
     // RoadPos에 Monster가 생성된다. (Invoke Repeat)
@@ -32,14 +34,34 @@ public class GameManager : MonoBehaviour
         MakeTree();
     }
 
+    //void MakeRoad()
+    //{
+    //    int spawnAmount = Random.Range(3, 6); //3이상 6미만 횟수만큼 생성이 반복된다.
+    //    for (int i = 0; i < spawnAmount; i++)
+    //    {
+    //        int[] spawnYPosArr = new int[] { -1, 0, 1, 2, 3, 4 };
+    //        Vector3[] spawnPosArr = new Vector3[spawnAmount];
+
+    //        for (int j = 0; j < spawnYPosArr.Length; j++)
+    //        {
+    //            int spawnYPos = spawnYPosArr[Random.Range(0, spawnYPosArr.Length)]; //randomYPos변수에 spawnYPosArr중 한 값이 랜덤으로 저장됨
+    //            Vector3 spawnPos = new Vector3(0, spawnYPos, 0);
+    //            spawnPosArr[j] = spawnPos;
+    //            Instantiate(road, spawnPosArr[j], Quaternion.identity);
+    //        }
+    //    }
+    //}
     void MakeRoad()
     {
-        int spawnAmount = Random.Range(3, 6); //3이상 6미만 횟수만큼 생성이 반복된다.
-        for (int i = 0; i < spawnAmount; i++)
+        int spawnAmount = Random.Range(3, 6); //스폰할 양(랜덤)
+        List<int> spawnYPosList = new List<int> { -1, 0, 1, 2, 3, 4 }; //스폰될 Y위치값 int 배열
+
+        for (int j = 0; j < spawnAmount; j++) //스폰될 양만큼 반복하여 생성
         {
-            int spawnYPos = Random.Range(-1, 5);
-            Vector3 spawnPos = new Vector3(0, spawnYPos, 0);
-            Instantiate(road, spawnPos, Quaternion.identity);
+            int spawnYPos = spawnYPosList[Random.Range(0, spawnYPosList.Count)]; //randomYPos에 랜덤하게 특정 스폰Y값이 배정됨
+            Vector3 spawnPos = new Vector3(0, spawnYPos, 0); //스폰 위치 세팅 
+            spawnYPosList.Remove(spawnYPos); //위치값이 겹치지 않도록 배열에서 값을 삭제.
+            Instantiate(road, spawnPos, Quaternion.identity); //생성
         }
     }
 
