fe15d6eac37dd24a5c7295cbd8687b6176702460
leehokyun <urea42@naver.com>
2024-11-12 14:39:17 +0900

    [주석 정리]

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,22 +12,9 @@ public class GameManager : MonoBehaviour
     // 스폰될 Y값
     Vector3 spawnPos1 = new Vector3(0, 0, 0);
     Vector3 spawnPos2 = new Vector3(0, 0, 0);
-    //Transform spawnTransform;
 
     List<int> spawnYPosList = new List<int>(); 
     List<int> spawnedRoadYPosList = new List<int>(); //몬스터 생성할 위치 찾기위해서 생성된 로드 위치 담아놓기
-
-    // Road가 3~5개 생성되어야한다.(완료)
-    //Random range 3~5사이의 변수만큼 for문이 돈다. (완료)
-    //그 횟수만큼 Road가 Instantiate된다. (완료)
-    //spawnYPos가 겹치지 않아야한다. 그러기위해서는 배열값들이 한번 쓸때마다 제거되어야한다.
-
-
-    // Road마다의 위치 값을 받아와야 한다.(전역변수)
-    // RoadPos에 Monster가 생성된다. (Invoke Repeat)
-
-    // Tree로 한줄이 꽉차는 상황에 대해서도 예외처리를 해야하지 않나.
-
 
     private void Start()
     {
@@ -74,7 +61,6 @@ public class GameManager : MonoBehaviour
     {
         int spawnAmount = Random.Range(6, 11); //스폰할 양(랜덤)
         List<int> spawnXPosList = new List<int> { -2, -1, 0, 1, 2 }; //스폰될 X위치값 int 배열
-        //List<int> spawnYPosList = new List<int> { -1, 0, 1, 2, 3, 4 }; //스폰될 Y위치값 int 배열은 Road 생성 뒤에 남은 값을 쓴다?
         List<Vector3> spawnPosList = new List<Vector3>();
 
         for (int i = 0; i < spawnAmount; i++)
